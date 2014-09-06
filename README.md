19b8ef135a9c83d7dd93e6a69c937fd04bdf7044
Ryan Rogers <ryan@timewasted.me>
2014-09-05 19:28:17 -0700

    Update to check for deprecated certificate signature algorithms.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 go-check-certs
 ==============
 
-This is a simple utility written in Go to check the validity and expiration dates of SSL certificates for a list of given hosts. Each certificate in the host's certificate chain is checked. By default, it will warn you if a certificate is going to expire within 30 days. Usage looks something like:
+This is a simple utility written in Go to check SSL certificates for a list of hosts. Each certificate in the host's certificate chain is checked for the following:
+
+* Expiration date. By default, you will be warned if a certificate will expire within 30 days. This can be adjusted with `-years=X`, `-months=X`, and/or `-days=X`.
+* Signature algorithm. Some algorithms have already been sunset, others are in the process of being sunset. This can be spammy, so you can disable the check with `-check-sig-alg=false`.
+
+Usage looks something like:
 
 ```
 ./go-check-certs -hosts="./path/to/file/with/hosts"
@@ -12,8 +17,8 @@ The hosts file is simply a single `hostname:port` per line. Empty lines or lines
 Current limitations:
 --------------------
 
-* This uses the host's root CA set to check the validity of certificates.  This means that, by default, it is not able to validate things like self-signed certificates.
-* A certificate must be valid for the expiration date to be checked.
+* This uses the host's root CA set to check the validity of certificates.  This means that it is not able to validate things like self-signed certificates.
+* A certificate must be valid for it to be checked.
 
 License:
 --------
--- check-certs.go
+++ check-certs.go
@@ -6,7 +6,9 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"strings"
@@ -16,23 +18,64 @@ import (
 
 const defaultConcurrency = 8
 
+const (
+	errExpiringShortly = "%s: ** '%s' (S/N %X) expires in %d hours! **"
+	errExpiringSoon    = "%s: '%s' (S/N %X) expires in roughly %d days."
+	errSunsetAlg       = "%s: '%s' (S/N %X) expires after the sunset date for its signature algorithm '%s'."
+)
+
+type sigAlgSunset struct {
+	name      string    // Human readable name of signature algorithm
+	sunsetsAt time.Time // Time the algorithm will be sunset
+}
+
+// sunsetSigAlgs is an algorithm to string mapping for signature algorithms
+// which have been or are being deprecated.  See the following links to learn
+// more about SHA1's inclusion on this list.
+//
+// - https://technet.microsoft.com/en-us/library/security/2880823.aspx
+// - http://googleonlinesecurity.blogspot.com/2014/09/gradually-sunsetting-sha-1.html
+var sunsetSigAlgs = map[x509.SignatureAlgorithm]sigAlgSunset{
+	x509.MD2WithRSA: sigAlgSunset{
+		name:      "MD2 with RSA",
+		sunsetsAt: time.Now(),
+	},
+	x509.MD5WithRSA: sigAlgSunset{
+		name:      "MD5 with RSA",
+		sunsetsAt: time.Now(),
+	},
+	x509.SHA1WithRSA: sigAlgSunset{
+		name:      "SHA1 with RSA",
+		sunsetsAt: time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
+	},
+	x509.DSAWithSHA1: sigAlgSunset{
+		name:      "DSA with SHA1",
+		sunsetsAt: time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
+	},
+	x509.ECDSAWithSHA1: sigAlgSunset{
+		name:      "ECDSA with SHA1",
+		sunsetsAt: time.Date(2017, 1, 1, 0, 0, 0, 0, time.UTC),
+	},
+}
+
 var (
 	hostsFile   = flag.String("hosts", "", "The path to the file containing a list of hosts to check.")
 	warnYears   = flag.Int("years", 0, "Warn if the certificate will expire within this many years.")
 	warnMonths  = flag.Int("months", 0, "Warn if the certificate will expire within this many months.")
 	warnDays    = flag.Int("days", 0, "Warn if the certificate will expire within this many days.")
+	checkSigAlg = flag.Bool("check-sig-alg", true, "Verify that non-root certificates are using a good signature algorithm.")
 	concurrency = flag.Int("concurrency", defaultConcurrency, "Maximum number of hosts to check at once.")
 )
 
-type certExpiration struct {
+type certErrors struct {
 	commonName string
-	expiresAt  time.Time
+	errs       []error
 }
 
 type hostResult struct {
 	host  string
 	err   error
-	certs []certExpiration
+	certs []certErrors
 }
 
 func main() {
@@ -81,20 +124,14 @@ func processHosts() {
 		close(results)
 	}()
 
-	timeNow := time.Now()
 	for r := range results {
 		if r.err != nil {
 			log.Printf("%s: %v\n", r.host, r.err)
 			continue
 		}
 		for _, cert := range r.certs {
-			if timeNow.AddDate(*warnYears, *warnMonths, *warnDays).After(cert.expiresAt) {
-				expiresIn := int64(cert.expiresAt.Sub(timeNow).Hours())
-				if expiresIn <= 48 {
-					log.Printf("%s: ** %s expires in %d hours! **\n", r.host, cert.commonName, expiresIn)
-				} else {
-					log.Printf("%s: %s expires in roughly %d days.\n", r.host, cert.commonName, expiresIn/24)
-				}
+			for _, err := range cert.errs {
+				log.Println(err)
 			}
 		}
 	}
@@ -137,7 +174,8 @@ func processQueue(done <-chan struct{}, hosts <-chan string, results chan<- host
 
 func checkHost(host string) (result hostResult) {
 	result = hostResult{
-		host: host,
+		host:  host,
+		certs: []certErrors{},
 	}
 	conn, err := tls.Dial("tcp", host, nil)
 	if err != nil {
@@ -146,16 +184,36 @@ func checkHost(host string) (result hostResult) {
 	}
 	defer conn.Close()
 
+	timeNow := time.Now()
 	checkedCerts := make(map[string]struct{})
 	for _, chain := range conn.ConnectionState().VerifiedChains {
-		for _, cert := range chain {
+		for certNum, cert := range chain {
 			if _, checked := checkedCerts[string(cert.Signature)]; checked {
 				continue
 			}
 			checkedCerts[string(cert.Signature)] = struct{}{}
-			result.certs = append(result.certs, certExpiration{
+			cErrs := []error{}
+
+			// Check the expiration.
+			if timeNow.AddDate(*warnYears, *warnMonths, *warnDays).After(cert.NotAfter) {
+				expiresIn := int64(cert.NotAfter.Sub(timeNow).Hours())
+				if expiresIn <= 48 {
+					cErrs = append(cErrs, fmt.Errorf(errExpiringShortly, host, cert.Subject.CommonName, cert.SerialNumber, expiresIn))
+				} else {
+					cErrs = append(cErrs, fmt.Errorf(errExpiringSoon, host, cert.Subject.CommonName, cert.SerialNumber, expiresIn/24))
+				}
+			}
+
+			// Check the signature algorithm, ignoring the root certificate.
+			if alg, exists := sunsetSigAlgs[cert.SignatureAlgorithm]; *checkSigAlg && exists && certNum != len(chain)-1 {
+				if cert.NotAfter.Equal(alg.sunsetsAt) || cert.NotAfter.After(alg.sunsetsAt) {
+					cErrs = append(cErrs, fmt.Errorf(errSunsetAlg, host, cert.Subject.CommonName, cert.SerialNumber, alg.name))
+				}
+			}
+
+			result.certs = append(result.certs, certErrors{
 				commonName: cert.Subject.CommonName,
-				expiresAt:  cert.NotAfter,
+				errs:       cErrs,
 			})
 		}
 	}
